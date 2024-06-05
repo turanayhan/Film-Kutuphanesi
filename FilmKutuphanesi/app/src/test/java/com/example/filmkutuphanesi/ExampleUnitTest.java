2d16379f11d55f4f159a1aa0c7ec5017a980ac8b
turan <duranayhan57@gmail.com>
2024-06-05 11:12:36 +0300

    v

--- FilmKutuphanesi/app/src/test/java/com/example/filmkutuphanesi/ExampleUnitTest.java
+++ FilmKutuphanesi/app/src/test/java/com/example/filmkutuphanesi/ExampleUnitTest.java
@@ -1,10 +1,15 @@
 package com.example.filmkutuphanesi;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import android.content.Context;
 
 import com.example.filmkutuphanesi.Adapter.FilmAdapter;
+import com.example.filmkutuphanesi.Database.SessionManager;
 import com.example.filmkutuphanesi.model.Film;
 
 import java.util.ArrayList;
@@ -19,16 +24,44 @@ public class ExampleUnitTest {
 
     @Test
     public void testFilmAdapter() {
-        // Test için film listesi oluşturuluyor
+
         List<Film> filmList = new ArrayList<>();
         filmList.add(new Film("Once Upon a Time in Hollywood", "Quentin Tarantino", new ArrayList<>(), "Drama", 2019, 161));
         filmList.add(new Film("Forrest Gump", "Robert Zemeckis", new ArrayList<>(), "Drama", 1994, 142));
 
-        // FilmAdapter örneği oluşturuluyor
+
         FilmAdapter adapter = new FilmAdapter(filmList);
 
-        // getItemCount() metodu doğru sayıyı döndürüyor mu kontrol ed
+
         assertEquals(filmList.size(), adapter.getItemCount());
 
     }
+
+
+
+    @Test
+    public void testSingletonInstance() {
+        // Gerçek bir Context nesnesi oluşturduk
+        Context context = Mockito.mock(Context.class);
+
+        // İlk çağrıda null dönmeli
+        assertNull(SessionManager.getInstance(null));
+
+        // İlk çağrıda bir instance oluşturulmalı
+        assertNotNull(SessionManager.getInstance(context));
+
+        // İkinci çağrıda ilk oluşturulan instance geri dönmeli
+        assertSame(SessionManager.getInstance(context), SessionManager.getInstance(context));
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
